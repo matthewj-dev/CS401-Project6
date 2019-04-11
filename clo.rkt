86e94d7a06feab16ef29f7e7c11f883fc8383ccb
Unknown <mjacks36@outlook.com>
2019-04-11 11:41:48 -0500

    add most of the functions. Tests are broken locally now though...

--- clo.rkt
+++ clo.rkt
@@ -23,7 +23,7 @@
 ;
 ; Honor pledge (please write your name.)
 ;  	
-; I **firstname lastname** have completed this code myself, without  	
+; I **Matthew Jackson** have completed this code myself, without  	
 ; unauthorized assistance, and have followed the academic honor code.
 ;
 ; Edit the code below to complete your solution. 
@@ -42,44 +42,61 @@
 ; prim-op is a prim?
 
 
-(define (prim? op)  	
-  (if (member op '(< = <= + - * vector vector-ref))  	
-      #t
-      #f))  	
+(define (prim? op) 
+  (if (member op '(< = <= + - * vector vector-ref)) 
+      #t   	
+      #f))
 
 
-(define (closure-convert e) 
-  ; Bottom up takes an expression and current list of procs, and returns a list
-  ; `(,e+ ,freee+ ,procs+) of the converted expression, its free variables, and a possibly-updated list of procs 	  
-  (define (bottom-up e procs)	
-    (match e  	
-           [`(let ([,x ,rhs]) ,bdy)  	
-            (match-define `(,rhs+ ,free0 ,procs0) 	  
-                          (bottom-up rhs procs)) 	  
-            (match-define `(,bdy+ ,free1 ,procs1) 
-                          (bottom-up bdy procs0)) 	  
-            `((let ([,x ,rhs+]) ,bdy+)
-              ,(set-union free0 (set-remove free1 x))	
-              ,procs1)] 
-            ; TODO: Add your other cases here
-            [(? number? n) 
-            `(,n ,(set) ,procs)]
-            [(? symbol? x)
-            `(,x ,(set x) ,procs)]
-            ; other cases
-            [`(,aes ...)
-            (clo-app ,aes ..)
+(define (closure-convert e)
+  ; Bottom up takes an expression and current list of procs, and returns a list 
+  ; `(,e+ ,free+ ,procs+) of the converted expression, its free variables, and a possibly-updated list of procs
+  (define (bottom-up e procs)
+    (match e
+          [`(let ([,x ,rhs]) ,bdy) 	
+          (match-define `(,rhs+ ,free0 ,procs0) 	
+                        (bottom-up rhs procs))
+          (match-define `(,bdy+ ,free1 ,procs1)   	
+                        (bottom-up bdy procs0))
+          `((let ([,x ,rhs+]) ,bdy+)   	
+            ,(set-union free0 (set-remove free1 x))
+            ,procs1)] 	
+            ; TODO: Add your other cases here   	
+          [(? number? n)
+          `(,n ,(set) ,procs)]
+          [(? symbol? x)
+          `(,x ,(set x) ,procs)]
+	        ; other cases
+          [`(lambda ,xs ,bdy)
+            (match-define `(,bdy+ ,free+ ,procs+) (bottom-up bdy procs))
+            (define free++ (set-subtract free+ (list->set xs)))
+            (define ordered-free-vars (set->list free++))
+            (define lam (gensym 'lam))
+            (define env (gensym 'env))
+              `((vector ,lam . ,ordered-free-vars)
+            ,free++
+            ,(cons `(proc (,lam ,env . ,xs)
+                      ,(cdr (foldl (lambda (x acc)
+                                (match-define (cons i bdy+) acc)
+                      (cons (+ 1 i)
+                          `(let ([,x (vector-ref ,env ,i)])
+                            ,bdy+)))
+                          (cons 1 bdy+)
+                    ordered-free-vars)))
+                  procs+))]
+          [`(,aes ...)
             (foldr (lambda (ae acc)
-              (match-define `((clo-app ,aes+) ,free0 ,procs) acc)
-              (match-define `(,ae+ ,free1 ,procs) 
-                              (bottom-up ae procs))
-              `((clo-app ae+ aes+) ,(set-union free0 free1) ,procs+))
+                    (match-define `((clo-app ,aes+ ...) ,free0 ,procs) acc)
+              (match-define `(,ae+ ,free1 ,procs+)
+                            (bottom-up ae procs))
+                    `((clo-app ,ae+ . ,aes+) ,(set-union free0 free1) ,procs+))
             `((clo-app) ,(set) ,procs)
-            aes)])) 
-  ; Use bottom-up to generate a main procedure from the program expression e  	
-  (match-define `(,main-body ,freevars ,procs)  	
-                (bottom-up e '()))  	
-  `((proc (main) ,main-body) . ,procs))  	
+            aes)]
+            )) 	
+  ; Use bottom-up to generate a main procedure from the program expression e 	
+  (match-define `(,main-body ,freevars ,procs)
+                (bottom-up e '()))
+  `((proc (main) ,main-body) . ,procs)) 
 
 
 ;;; Output Language
